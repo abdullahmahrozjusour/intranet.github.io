88b854658e23e1b0827e7aa03959c3778b001538
Abdullah Mahroz <a.mahroz@jusour.qa>
2024-11-26 12:56:11 +0300

    update directory

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,10 +1,496 @@
-const suggestions = [
-    { name: "John Doe", contact: "555-0123", email: 'info@gmail.com' },
+const suggestions2 = [
+    { name: "A.Hadi Bakhit H Barqan ", contact: "44011077", email: 'a.hadi@jusour.qa' },
     { name: "Jane Smith", contact: "555-0456", email: 'info@gmail.com' },
     { name: "Mike Johnson", contact: "555-0789", email: 'info@gmail.com' },
     { name: "Emily Davis", contact: "555-1234", email: 'info@gmail.com' },
     { name: "Chris Brown", contact: "555-5678", email: 'info@gmail.com' }
 ];
+
+
+const suggestions = [
+    {
+        firstName: "A.Hadi Bakhit H ",
+        lastName: "Barqan ",
+        contact: "44011077",
+        email: "a.hadi@jusour.qa"
+    },
+    {
+        firstName: "Sarah",
+        lastName: "Al-Omari",
+        contact: "44011001",
+        email: "sara@jusour.qa"
+    },
+    {
+        firstName: "Marina",
+        lastName: "Ayoub",
+        contact: "44011002",
+        email: "m.ayoub@jusour.qa"
+    },
+    {
+        firstName: "Saadia",
+        lastName: "Rabah",
+        contact: "44011003",
+        email: "s.rabah@jusour.qa"
+    },
+    {
+        firstName: "Ghenna",
+        lastName: "Kanawati",
+        contact: "44011004",
+        email: "g.kanawati@jusour.qa"
+    },
+    {
+        firstName: "Bakhita",
+        lastName: "Maqarih",
+        contact: "44011005",
+        email: "b.maqarih@jusour.qa"
+    },
+    {
+        firstName: "Naseem",
+        lastName: "Alsawalhi",
+        contact: "44011006",
+        email: "n.alsawalhi@jusour.qa"
+    },
+    {
+        firstName: "Kaltham",
+        lastName: "Al-Boenain",
+        contact: "44011007",
+        email: "k.alboenain@jusour.qa"
+    },
+    {
+        firstName: "Ibrahim",
+        lastName: "Mulla",
+        contact: "44011008",
+        email: "ibrahim.almulla@jusour.qa"
+    },
+    {
+        firstName: "Awad",
+        lastName: "Eldafari",
+        contact: "44011009",
+        email: "awad.ahmed@jusour.qa"
+    },
+    {
+        firstName: "Mashael",
+        lastName: "Al-Muhannadi",
+        contact: "44011010",
+        email: "mashael@jusour.qa"
+    },
+    {
+        firstName: "Dr.Hezam",
+        lastName: " Al-Meqareh",
+        contact: "44011011",
+        email: "hezam@jusour.qa"
+    },
+    {
+        firstName: "Hassan",
+        lastName: "Eissa",
+        contact: "44011012",
+        email: " h.eissa@jusour.qa"
+    },
+    {
+        firstName: "Marwa",
+        lastName: "Semhat",
+        contact: "44011013",
+        email: "m.semhat@jusour.qa"
+    },
+    {
+        firstName: "Abdulkhafiz",
+        lastName: "Azizov",
+        contact: "44011014",
+        email: "a.azizov@jusour.qa"
+    },
+    {
+        firstName: "Maryam",
+        lastName: "Al-Sorour",
+        contact: "44011015",
+        email: "m.alsorour@jusour.qa"
+    },
+    {
+        firstName: "Huda",
+        lastName: "Al-Sultan",
+        contact: "44011016",
+        email: "h.alsultan@jusour.qa"
+    },
+    {
+        firstName: "Suma",
+        lastName: "Paul",
+        contact: "44011017",
+        email: "spaul@jusour.qa"
+    },
+    {
+        firstName: "Faris",
+        lastName: "Ahmed",
+        contact: "44011018",
+        email: "f.ahmed@jusour.qa"
+    },
+    {
+        firstName: "Ali ",
+        lastName: "Hazim",
+        contact: "44011020",
+        email: "a.hazim@jusour.qa"
+    },
+    {
+        firstName: "Aljazi",
+        lastName: "Al-Marri",
+        contact: "44011021",
+        email: "aljazi.almarri@jusour.qa"
+    },
+    {
+        firstName: "Maha",
+        lastName: "Al-Muhannadi",
+        contact: "44011022",
+        email: "maha.almuhannadi@jusour.qa"
+    },
+    {
+        firstName: "Rawan",
+        lastName: "AL Zoubi ",
+        contact: "44011023",
+        email: "r.alzoubi@jusour.qa"
+    },
+    {
+        firstName: "Emad",
+        lastName: " Hassan",
+        contact: "44011024",
+        email: "e.hassan@jusour.qa"
+    },
+    {
+        firstName: "Razan",
+        lastName: " Abuagla ",
+        contact: "44011025",
+        email: "r.abuagla@jusour.qa"
+    },
+    {
+        firstName: "Mohd",
+        lastName: " Alhams",
+        contact: "44011026",
+        email: "m.alhams@jusour.qa"
+    },
+    {
+        firstName: "Jawaher",
+        lastName: "AL-Jehani  ",
+        contact: "44011027",
+        email: "j.aljehani@jusour.qa"
+    },
+    {
+        firstName: "Wadha",
+        lastName: "Al-Khayarin",
+        contact: "44011028",
+        email: "W.Alkhayarin@jusour.qa"
+    },
+    {
+        firstName: "Abdulmajeed",
+        lastName: "Al Shemmari",
+        contact: "44011029",
+        email: "a.alshemmari@jusour.qa"
+    },
+    {
+        firstName: "Naser",
+        lastName: " Alhajri",
+        contact: "44011030",
+        email: "n.alhajri@jusour.qa"
+    },
+    {
+        firstName: "Toka",
+        lastName: "Maghreby",
+        contact: "44011031",
+        email: "t.almaghraby@jusour.qa"
+    },
+    {
+        firstName: "Sifeddine",
+        lastName: "Chaiter",
+        contact: "44011033",
+        email: "sif.eddine@jusour.qa"
+    },
+    {
+        firstName: "Walid",
+        lastName: "Bourbia",
+        contact: "44011034",
+        email: "walid@jusour.qa"
+    },
+    {
+        firstName: "Khalood",
+        lastName: "Al-Emadi",
+        contact: "44011035",
+        email: "k.alemadi@jusour.qa"
+    },
+    {
+        firstName: "Mustafa",
+        lastName: "Qutbuddin",
+        contact: "44011036",
+        email: "m.qutbuddin@jusour.qa"
+    },
+    {
+        firstName: "Tariq",
+        lastName: "Al-Bqour",
+        contact: 44011037,
+        email: "t.albqour@jusour.qa"
+    },
+    {
+        firstName: "Sofian",
+        lastName: " Babiker",
+        contact: 44011038,
+        email: "sofian.babiker@jusour.qa"
+    },
+    {
+        firstName: "Mohd",
+        lastName: " Shyyab",
+        contact: 44011039,
+        email: "m.shyyab@jusour.qa"
+    },
+    {
+        firstName: "Montasir",
+        lastName: " Abdelgali",
+        contact: 44011039,
+        email: "N\/A"
+    },
+    {
+        firstName: "Mohammad",
+        lastName: "Alkharabsheh",
+        contact: 44011040,
+        email: "m.alkharabsheh@jusour.qa"
+    },
+    {
+        firstName: "Rami",
+        lastName: "Saidi",
+        contact: 44011041,
+        email: "rami.saidi@jusour.qa"
+    },
+    {
+        firstName: "Mohammed",
+        lastName: "Al Den",
+        contact: 44011042,
+        email: "m.alden@jusour.qa"
+    },
+    {
+        firstName: "Dania",
+        lastName: "Aljandaly",
+        contact: 44011043,
+        email: "d.aljandaly@jusour.qa"
+    },
+    {
+        firstName: "Amin",
+        lastName: "Benhida",
+        contact: 44011045,
+        email: "a.benhida@jusour.qa"
+    },
+    {
+        firstName: "Mohammed",
+        lastName: " Abozaid",
+        contact: 44011046,
+        email: "m.abozaid@jusour.qa"
+    },
+    {
+        firstName: "Ayan",
+        lastName: " Khalif",
+        contact: 44011047,
+        email: "a.khalif@jusour.qa"
+    },
+    {
+        firstName: "Chellappan",
+        lastName: "Sunil Kumar",
+        contact: 44011052,
+        email: "c.kumar@jusour.qa"
+    },
+    {
+        firstName: "Sara",
+        lastName: "ElKurdi",
+        contact: 44011055,
+        email: "s.elkurdi@jusour.qa"
+    },
+    {
+        firstName: "Abir",
+        lastName: "Chikh",
+        contact: 44011056,
+        email: "a.chikh@jusour.qa"
+    },
+    {
+        firstName: "Rahees",
+        lastName: "Pallivalappil",
+        contact: 44011061,
+        email: "N\/A"
+    },
+    {
+        firstName: "Januka",
+        lastName: "Dhital",
+        contact: 44011062,
+        email: "N\/A"
+    },
+    {
+        firstName: "Noushad",
+        lastName: "Mavuppadi",
+        contact: 44011063,
+        email: "N\/A"
+    },
+    {
+        firstName: "Sapana",
+        lastName: "Darji",
+        contact: 44011063,
+        email: "N\/A"
+    },
+    {
+        firstName: "Salim",
+        lastName: "Bouzekouk",
+        contact: 44011070,
+        email: "s.bouzekouk@jusour.qa"
+    },
+    {
+        firstName: "Mohammed",
+        lastName: "Ummer",
+        contact: 44011071,
+        email: "m.ummer@jusour.qa"
+    },
+    {
+        firstName: "Zaid",
+        lastName: "Mansoor",
+        contact: 44011071,
+        email: "z.mansoor@jusour.qa"
+    },
+    {
+        firstName: "Eric",
+        lastName: "Musobo",
+        contact: 44011071,
+        email: "b.musobo@jusour.qa"
+    },
+    {
+        firstName: "Khireddine",
+        lastName: "Garri",
+        contact: 44011074,
+        email: "K.Garri@jusour.qa"
+    },
+    {
+        firstName: "Dana",
+        lastName: " Al-Dafaa",
+        contact: 44011080,
+        email: "d.aldafaa@jusour.qa"
+    },
+    {
+        firstName: "Ala'-Aldin",
+        lastName: "Abdullah",
+        contact: 44011081,
+        email: "a.abdullah@jusour.qa"
+    },
+    {
+        firstName: "Amna",
+        lastName: " Zaarab",
+        contact: 44011082,
+        email: "a.zaarab@jusour.qa"
+    },
+    {
+        firstName: "Toufik",
+        lastName: "Lessak",
+        contact: 44011083,
+        email: "t.lessak@jusour.qa"
+    },
+    {
+        firstName: "Mona",
+        lastName: "Msaddar",
+        contact: 44011084,
+        email: "m.almsaddar@jusour.qa"
+    },
+    {
+        firstName: "Afrah",
+        lastName: "Al-Fardi",
+        contact: 44011085,
+        email: "a.alfardi@jusour.qa"
+    },
+    {
+        firstName: "Sharif",
+        lastName: "Shaikh",
+        contact: 44011086,
+        email: "s.shaikh@jusour.qa"
+    },
+    {
+        firstName: "Yusra ",
+        lastName: "Diab",
+        contact: 44011087,
+        email: "y.diab@jusour.qa"
+    },
+    {
+        firstName: "Amin",
+        lastName: "Matni",
+        contact: 44011093,
+        email: "a.matni@jusour.qa"
+    },
+    {
+        firstName: "Qais",
+        lastName: " Aburub",
+        contact: 44011094,
+        email: "q.aburub@jusour.qa"
+    },
+    {
+        firstName: "Mohammad",
+        lastName: "Falamarzi",
+        contact: 44011096,
+        email: "m.falamarzi@jusour.qa"
+    },
+    {
+        firstName: "Abdulla",
+        lastName: "Al-Shahwani",
+        contact: "N\/A",
+        email: "a.alshahwani@jusour.qa"
+    },
+    {
+        firstName: "Alya",
+        lastName: "Al-Kuwari",
+        contact: "N\/A",
+        email: "alya.alkuwari@jusour.qa"
+    },
+    {
+        firstName: "Muhammad",
+        lastName: "Khalid ",
+        contact: "N\/A",
+        email: "m.khalid@jusour.qa"
+    },
+    {
+        firstName: "Sunny",
+        lastName: "Haque",
+        contact: "N\/A",
+        email: "s.haque@jusour.qa"
+    },
+    {
+        firstName: "Abdullah",
+        lastName: "Mahrooz",
+        contact: "N\/A",
+        email: "a.mahroz@jusour.qa"
+    },
+    {
+        firstName: "Nasser ",
+        lastName: "Al-Shaiba",
+        contact: "N\/A",
+        email: "N\/A"
+
+    },
+    {
+        firstName: "Krishna",
+        lastName: "Kapadisudi",
+        contact: "N\/A",
+        email: "k.prasadkapadisudi@jusour.qa",
+    },
+    {
+        firstName: "Ansal",
+        lastName: "Majeed",
+        contact: "N\/A",
+        email: "N\/A"
+    },
+    {
+        firstName: "Top",
+        lastName: "Bhattarai",
+        contact: "N\/A",
+        email: "N\/A"
+    },
+    {
+        firstName: "Khalid ",
+        lastName: "Aljamali",
+        contact: "N\/A",
+        email: "N\/A"
+    },
+    {
+        firstName: "Faisal ",
+        lastName: "Al-Mansoori",
+        contact: "N\/A",
+        email: "N\/A"
+    }
+]
+
 
 const input = document.getElementById('autocomplete-input');
 const suggestionsContainer = document.getElementById('suggestions');
@@ -18,9 +504,10 @@ input.addEventListener('input', () => {
         return;
     }
 
-    const filteredSuggestions = suggestions.filter(item => 
-        item.name.toLowerCase().includes(query) || 
-        item.contact.includes(query) || 
+    const filteredSuggestions = suggestions.filter(item =>
+        item.firstName.toLowerCase().includes(query) ||
+        item.lastName.toLowerCase().includes(query) ||
+        // item.contact.includes(query) ||
         item.email.toLowerCase().includes(query)
     );
 
@@ -29,12 +516,13 @@ input.addEventListener('input', () => {
             const div = document.createElement('div');
             div.classList.add('suggestion-item');
             div.innerHTML = `
-                <div class="font-bold">${item.name}</div>
+                <div class="font-bold">${item.firstName} ${item.lastName}</div>
                 <div class="">${item.contact}</div>
                 <div class="">${item.email}</div>
             `;
             div.addEventListener('click', () => {
-                input.value = item.name;
+                input.value = item.firstName;
+                input.value = item.lastName;
                 suggestionsContainer.innerHTML = '';
                 suggestionsContainer.style.display = 'none';
             });
@@ -52,7 +540,7 @@ document.addEventListener('click', (event) => {
     }
 });
 
-$(document).ready(function(){
+$(document).ready(function () {
     $('.nav-menu-link').on('click', function (e) {
         e.preventDefault()
         var id = $(this).attr('href')
@@ -60,9 +548,9 @@ $(document).ready(function(){
         $(id).slideToggle();
     })
 
-    $('.toggle-button').on('click', function(e){
+    $('.toggle-button').on('click', function (e) {
         e.preventDefault()
-        var id = $(this).attr('href') 
+        var id = $(this).attr('href')
         $(this).find('.fa-chevron-down').toggleClass('rotate-180')
         $(id).slideToggle('fast');
     })
